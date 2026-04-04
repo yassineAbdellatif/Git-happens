0fdf0d80aa5fe7dc4b282dc8589efd8ff4de8e46
Khaled Daoud <khaleddaoud@Khaleds-MacBook-Pro-2.local>
2026-04-04 13:33:17 -0400

    fix: add firestore db export

--- app/frontend/src/features/auth/config/firebaseConfig.js
+++ app/frontend/src/features/auth/config/firebaseConfig.js
@@ -2,6 +2,7 @@
 import { initializeApp } from "firebase/app";
 import { initializeAuth, getReactNativePersistence } from 'firebase/auth';
 import ReactNativeAsyncStorage from '@react-native-async-storage/async-storage';
+import { getFirestore } from "firebase/firestore";
 
 
 // TODO: Add SDKs for Firebase products that you want to use
@@ -21,6 +22,9 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+
+export const db = getFirestore(app);
+
 export const auth = initializeAuth(app, {
   persistence: getReactNativePersistence(ReactNativeAsyncStorage)
 });
